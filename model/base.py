06dc4775fdaf4c7e03a62128e8fcf3294a15f531
Tanya W <warriertanya@gmail.com>
2025-11-20 20:00:33 +0530

    feat: adds dropout feature

--- model/base.py
+++ model/base.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 from abc import ABC, abstractmethod
 
 
-class BaseTemporalModel(nn.Module, ABC):
-    def __init__(self, in_channels, hidden_channels, num_relations=9):
+class BaseGCNModel(nn.Module, ABC):
+    def __init__(self, in_channels, hidden_channels, num_relations=9, dropout=0.3):
         super().__init__()
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
         self.num_relations = num_relations
-        
+        self.dropout = dropout
+
     @abstractmethod
     def forward(self):
         pass
-    
+
     def get_name(self):
         return self.__class__.__name__
