e01ccda7718ffea0990cdfba308cc6e7c05769ab
MadhavWalia <madhavwalia8139@gmail.com>
2025-11-22 01:10:18 +0800

    refactor: updated variables passed to abstract class

--- model/base.py
+++ model/base.py
@@ -4,15 +4,17 @@ from abc import ABC, abstractmethod
 
 
 class BaseGCNModel(nn.Module, ABC):
-    def __init__(self, in_channels, hidden_channels, num_relations=9, dropout=0.3):
+    def __init__(self, in_channels, hidden_channels, num_relations=2, out_channels=2, dropout=0.3, num_layers=2):
         super().__init__()
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
         self.num_relations = num_relations
         self.dropout = dropout
+        self.num_layers = num_layers
 
     @abstractmethod
-    def forward(self):
+    def forward(self, x, edge_index, edge_type):
         pass
 
     def get_name(self):
