8a0bf7bc4de2831d3af548dd5b32737392535a41
Tanya W <warriertanya@gmail.com>
2025-11-22 17:33:41 +0530

    feat: improve botrgcn architecture

--- model/bot_rgcn.py
+++ model/bot_rgcn.py
@@ -6,17 +6,20 @@ from .base import BaseGCNModel
 
 
 class BotRGCN(BaseGCNModel):
+
     def __init__(
         self,
         in_channels=128,  # node embeddings dim
         hidden_channels=128,
         num_relations=2,  # (0) followers, (1) following
-        dropout=0.3,
+        dropout=0.2,
     ):
 
         super().__init__(in_channels, hidden_channels, num_relations, dropout)
 
-        self.rgcn = RGCNConv(in_channels, hidden_channels, num_relations=num_relations)
+        self.rgcn1 = RGCNConv(in_channels, hidden_channels, num_relations=num_relations)
+
+        self.rgcn2 = RGCNConv(in_channels, hidden_channels, num_relations=num_relations)
 
         self.linear_relu = nn.Sequential(
             nn.Linear(hidden_channels, hidden_channels), nn.LeakyReLU()
@@ -31,12 +34,12 @@ class BotRGCN(BaseGCNModel):
         """
 
         # rgcn layer 1
-        x = self.rgcn(x, edge_index, edge_type)
+        x = self.rgcn1(x, edge_index, edge_type)
         x = F.leaky_relu(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
 
         # rgcn layer 2
-        x = self.rgcn(x, edge_index, edge_type)
+        x = self.rgcn2(x, edge_index, edge_type)
         x = F.leaky_relu(x)
 
         # MLP head
